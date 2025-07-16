09decf17c3640d8c51a5e538e8222c40c57e7293
Bryan Sandoval <bryansandoval@Bryans-MacBook-Air.local>
2025-07-15 21:10:48 -0700

    member class

---
+++ src/main/java/com/bryan/loonaapi/Member.java
@@ -0,0 +1,55 @@
+package com.bryan.loonaapi;
+
+import javax.persistence.Entity;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+public class Member {
+    private String name;
+    private String colorCode;
+    private localDate bornDate;
+
+    public Member(String name, String colorCode, LocalDate bornDate) {
+        this.name = name;
+        this.colorCode = colorCode;
+        this.bornDate = bornDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public localDate getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(localDate bornDate) {
+        this.bornDate = bornDate;
+    }
+
+    public int getAge() {
+        if (bornDate == null) {
+            return 0;
+        }
+        return Period.between(bornDate, LocalDate.now()).getYears();
+    }
+}
---
+++ src/main/java/com/bryan/loonaapi/MemberService.java
@@ -0,0 +1,26 @@
+package com.bryan.loonaapi;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.time.LocalDate;
+
+@Service
+public class MemberService {
+    public List<Member> getAllMembers() {
+        return List.of(
+                new Member("HeeJin", "FF4EB1", LocalDate.of(2000, 10, 19)),
+                new Member("HyunJin", "F5D600", LocalDate.of(2000, 11, 15)),
+                new Member("HaSeul", "00A86B", LocalDate.of(1997, 8, 18)),
+                new Member("YeoJin", "FF7F00", LocalDate.of(2002, 11, 11)),
+                new Member("ViVi", "FFB7D5", LocalDate.of(1996, 12, 9)),
+                new Member("Kim Lip", "FFB7D5", LocalDate.of(1999, 2, 10)),
+                new Member("JinSoul", "FFB7D5", LocalDate.of(1997, 6, 13)),
+                new Member("Choerry", "FFB7D5", LocalDate.of(2001, 6, 4)),
+                new Member("Yves", "FFB7D5", LocalDate.of(1997, 2, 24)),
+                new Member("Chuu", "FFB7D5", LocalDate.of(1999, 10, 20)),
+                new Member("GoWon", "FFB7D5", LocalDate.of(2000, 11, 19)),
+                new Member("HyeJu", "FFB7D5", LocalDate.of(2001, 11, 13))
+        );
+    }
+}
